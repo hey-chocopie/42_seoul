21f61696d46dfc965c18fdc6da8cce9798435840
Hoyong Lee <hoylee@student.42seoul.kr>
2021-05-10 22:41:31 +0900

    wordpress

--- ft_services/wordpress/srcs/wordpress/ft_wordpress.sh
+++ ft_services/wordpress/srcs/wordpress/ft_wordpress.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-rc-service lighttpd start && rc-update add lighttpd default
-#apk add wget php5-mysql mysql mysql-client php5-zlib
-/etc/init.d/lighttpd restart
-/bin/sh
+#!/bin/sh
+usr/sbin/php-fpm7
+nginx -g "daemon off;"
+#/bin/sh
