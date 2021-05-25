403951ef7cdc71c9555b3fb1b01b0b67c36875b0
hey-chocopie <ghdyd6372@naver.com>
2021-05-26 01:35:47 +0900

    makefile upload

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -20,6 +20,9 @@ typedef struct		s_sd
 	int		cb;
 }					t_sd;
 
+void	 A_to_B(t_list **stackA, t_list **stackB, t_sd *s_data, int range);
+void	 B_to_A(t_list **stackA, t_list **stackB, t_sd *s_data, int range);
+
 void		swap(t_list **start)
 {
 	int tmp;
@@ -62,12 +65,13 @@ int		 make_stack_a(int i, char **argv, t_list **start)
 		return -1;
 	tmp = ft_lstnew(value);
 	//말록한거라 start는 프리해줘야함.
-	printf("%d ",(*tmp).content);
-	if(i != 1)
+	//printf("%d ",(*tmp).content);
+	if((*start) != NULL)
 		ft_lstadd_back(start, tmp);
-	else if(i == 1)
+	else if((*start) == NULL)
+	{
 		*start = ft_lstnew(ft_atoi(argv[i]));
-
+	}
 	return 0;
 }
 
@@ -75,7 +79,7 @@ void		stack_d_check(t_list *stackA, t_list *stackB, t_sd *s_data)
 {
 	int i = 0;
 	//test 0~end stack A, stackB
-	printf("stackA : \n");
+	printf("\nstackA : ");
 //	if(stackA != 0)
 //	{
 		while(i <= s_data -> ca && s_data -> ca != 0)
@@ -86,7 +90,7 @@ void		stack_d_check(t_list *stackA, t_list *stackB, t_sd *s_data)
 		}
 //		printf("%d ", stackA->content);
 //	}
-	printf("stackB : \n");
+	printf("\nstackB : ");
 	i = 0;
 //	if(stackB != 0)
 //	{
@@ -133,17 +137,20 @@ void		circle_lst(t_list **stack, int lst_depth)
 void		sa(t_list **stackA)
 {
 	swap(stackA);
+	write(1, "sa\n", 3);
 }
 
 void		sb(t_list **stackB)
 {
 	swap(stackB);
+	write(1, "sb\n", 3);
 }
 
 void		ss(t_list **stackA, t_list **stackB)
 {
 	sa(stackA);
 	sb(stackB);
+	write(1, "ss\n", 3);
 }
 
 void		pa(t_list **stackA, t_list **stackB, t_sd *s_data)
@@ -155,7 +162,7 @@ void		pa(t_list **stackA, t_list **stackB, t_sd *s_data)
 	push(stackB, stackA);
 	circle_lst(stackA, s_data -> ca);
 	circle_lst(stackB, s_data -> cb);
-
+	write(1, "pa\n", 3);
 }
 
 void		pb(t_list **stackA, t_list **stackB, t_sd *s_data)
@@ -167,22 +174,27 @@ void		pb(t_list **stackA, t_list **stackB, t_sd *s_data)
 	push(stackA, stackB);
 	circle_lst(stackA, s_data -> ca);
 	circle_lst(stackB, s_data -> cb);
+	write(1, "pb\n", 3);
 }
 
 void	ra(t_list **stackA)
 {
 	(*stackA) = (*stackA) -> next;
+	write(1, "ra\n", 3);
 }
 
 void	rb(t_list **stackB)
 {
 	(*stackB) = (*stackB) -> next;
+	write(1, "rb\n", 3);
 }
 
 void	rr(t_list **stackA, t_list **stackB)
 {
 	ra(stackA);
 	rb(stackB);
+	write(1, "rr\n", 3);
+	// 수정..
 }
 
 void	rra(t_list **stackA, t_sd *s_data)
@@ -194,6 +206,7 @@ void	rra(t_list **stackA, t_sd *s_data)
 		*stackA = (*stackA) -> next;
 		i++;
 	}
+	write(1, "rra\n", 4);
 }
 
 void	rrb(t_list **stackB, t_sd *s_data)
@@ -202,16 +215,17 @@ void	rrb(t_list **stackB, t_sd *s_data)
 
 	while (i < s_data -> cb)
 	{
-		printf("\n%d\n ", i);
 		*stackB = (*stackB) -> next;
 		i++;
 	}
+	write(1, "rrb\n", 4);
 }
 
 void	rrr(t_list **stackA, t_list **stackB, t_sd *s_data)
 {
 	rra(stackA, s_data);
 	rrb(stackB, s_data);
+	write(1, "rrr\n", 4);
 }
 
 int		min_value(t_list **stack, int depth)
@@ -263,33 +277,96 @@ int		select_pivot(t_list **stack, int range)
 	int i = 0;
 	int avr;
 	t_list *tmp;
-	//avr = min_value(stack, range);
+
 	avr = avr_value(stack, range, min_value(stack, range));
-	printf("---%d----", avr);
 	return avr;
 }
 
 void	 A_to_B(t_list **stackA, t_list **stackB, t_sd *s_data, int range)
 {
 	int i = 0;
+	int pivot;
+	int ra_count = 0;
+	int pb_count = 0;
+
 	if (range == 1)
 		return ;
-	select_pivot(stackA, range);
+	pivot = select_pivot(stackA, range);
 	//스택A의 depth개의 원소 중에서 "적절한" 피봇을 선택한다
-//	for (i < depth)
-//	{
-//	}
-//	for (i < depth)
-//	{
-//		if (스택A의 top) > 피봇
-//			ra명령으로 뒤로 넘긴다
-//			ra_호출_횟수++
-//		else
-//			pb명령으로 b로 보낸다
-//			pb_호출_횟수++
-//	}
+
+	while (i < range)
+	{
+		if ((*stackA)->content > pivot)
+		{
+			ra(stackA);
+			ra_count++;
+		}
+		else
+		{
+			pb(stackA, stackB, s_data);
+			pb_count++;
+			//pb_호출_횟수++
+		}
+		i++;
+	}
+	i = 0;
+	while(i < ra_count)
+	{
+		rra(stackA, s_data);
+		i++;
+	}
+	//stack_d_check(*stackA, *stackB, s_data);
+	A_to_B(stackA, stackB, s_data, ra_count);
+	//stack_d_check(*stackA, *stackB, s_data);
+	B_to_A(stackA, stackB, s_data, pb_count);
 //	for ra_호출 횟수
 //		rra #ra로 넘어간 원소들을 다시 스택 상단으로 되감기
+//	A_to_B(ra_호출_횟수) #A를 최대한 가볍게 만든다
+//	B_to_A(pb_호출_횟수) #A의 정렬이 끝나면 B로 넘어간 것들을 A로 올린다.
+	return ;
+}
+
+
+void	 B_to_A(t_list **stackA, t_list **stackB, t_sd *s_data, int range)
+{
+	int i = 0;
+	int pivot;
+	int rb_count = 0;
+	int pa_count = 0;
+
+	if (range == 1)
+	{
+		pa(stackA, stackB, s_data);
+		return ;
+	}
+	pivot = select_pivot(stackB, range);
+	//스택B의 depth개의 원소 중에서 "적절한" 피봇을 선택한다
+
+	while (i < range)
+	{
+		if ((*stackB)->content <= pivot)
+		{
+			rb(stackB);
+			rb_count++;
+		}
+		else
+		{
+			pa(stackA, stackB, s_data);
+			pa_count++;
+			//pa_호출_횟수++
+		}
+		i++;
+	}
+	i = 0;
+	while(i < rb_count)
+	{
+		rrb(stackB, s_data);
+		i++;
+	}
+	//stack_d_check(*stackA, *stackB, s_data);
+	A_to_B(stackA, stackB, s_data, pa_count);
+	//stack_d_check(*stackA, *stackB, s_data);
+	B_to_A(stackA, stackB, s_data, rb_count);
 //	A_to_B(ra_호출_횟수) #A를 최대한 가볍게 만든다
 //	B_to_A(pb_호출_횟수) #A의 정렬이 끝나면 B로 넘어간 것들을 A로 올린다.
 	return ;
@@ -318,6 +395,15 @@ void	 A_to_B(t_list **stackA, t_list **stackB, t_sd *s_data, int range)
 // ps 
 // stack_d_check : 값들이 잘 들어가있는지 확인
 
+void arg_string(char ***argv, int *argc, int *i)
+{
+	(*argv) = ft_split((*argv)[1], ' ');
+	//스플릿은 free
+	*i = 0;
+	*argc = 0;
+	while((*argv)[(*argc)])
+		(*argc)++;
+}
 int main(int argc, char **argv)
 {
 	t_sd	s_data;
@@ -328,6 +414,8 @@ int main(int argc, char **argv)
 	setup_data(&s_data);
 	if(argc < 2)
 		return 0;
+	if(argc == 2)
+		arg_string(&argv, &argc, &i);
 	while(i != argc)
 	{
 		s_data.ca++;
@@ -335,27 +423,29 @@ int main(int argc, char **argv)
 			return -1;
 		i++;
 	}
-	//printf("%d", s_data.ca);
 	circle_lst(&stackA, s_data.ca);
-	//------------- testzone ------------------
-	//sa(&stackA);
-	//sb(&stackB);
-	//ss(&stackA, &stackB);
-	//pa(&stackA, &stackB, &s_data);
-
-	//ra, rb 테스트
-//	pb(&stackA, &stackB, &s_data);
-//	pb(&stackA, &stackB, &s_data);
-//	pb(&stackA, &stackB, &s_data);
-//	ra(&stackA);
-//	rb(&stackB);
 //
-//	rra(&stackA, &s_data);
-//	rrb(&stackB, &s_data);
-//	rrr(&stackA, &stackB, &s_data);
-	//-----------------------------------------
-//	stack_d_check(stackA, stackB, &s_data);
+//	//printf("%d", s_data.ca);
+//	//------------- testzone ------------------
+//	//sa(&stackA);
+//	//sb(&stackB);
+//	//ss(&stackA, &stackB);
+//	//pa(&stackA, &stackB, &s_data);
+//
+//	//ra, rb 테스트
+////	pb(&stackA, &stackB, &s_data);
+////	pb(&stackA, &stackB, &s_data);
+////	pb(&stackA, &stackB, &s_data);
+////	ra(&stackA);
+////	rb(&stackB);
+////
+////	rra(&stackA, &s_data);
+////	rrb(&stackB, &s_data);
+////	rrr(&stackA, &stackB, &s_data);
+//	//-----------------------------------------
 	A_to_B(&stackA, &stackB, &s_data, s_data.ca);
 	
+//	//stack_d_check(stackA, stackB, &s_data);
+//	//stack_d_check(stackA, stackB, &s_data);
 	return 0;
 }
