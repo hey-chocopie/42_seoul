f44fe92950e71d685238ba96671dc85339a724a7
hey-chocopie <ghdyd6372@naver.com>
2021-08-10 16:34:44 +0900

    cpp00 ex01

--- Cpp_Module/m00/ex01/awesome_phone_book.cpp
+++ Cpp_Module/m00/ex01/awesome_phone_book.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 int input_data(std::string *str)
 {
@@ -11,45 +12,97 @@ int input_data(std::string *str)
 class phone_book
 {
 public:
-	std::string first_name;
-	std::string last_name;
-	std::string nickname;
-	std::string phone_number;
-	std::string darkest_secret;
+	std::string str;
+	std::string book_value[5];
 
-	int ADD(std::string *kind_list, std::string *str, int *index)
+	int ADD(std::string *kind_list, int *index)
 	{
-		if (*index == 7)
-			*index = 0;
-		else
-			(*index)++;
+		std::cout << "input a new contact’s information" << std::endl;
+		if ((*index) / 8 > 0)
+			std::cout << "old contact delete and add new contact" << std::endl;
+		(*index)++;
 		for (int i = 0; i < 5; i++)
 		{
-			(*str) = "";
-			while((*str).length() == 0)
+			str = "";
+			while(str.length() == 0)
 			{
 				std::cout << kind_list[i] << " = ";
-				if(input_data(str) == -1)
+				if(input_data(&str) == -1)
 					return (-1);
-				if (str->length() == 0)
+				if (str.length() == 0)
 					std::cout << "Empty contact not added !" << std::endl;
 			}
-			if (i == 0)
-				first_name = *str;
-			else if(i == 1)
-				last_name = *str;
-			else if(i == 2)
-				nickname = *str;
-			else if(i == 3)
-				phone_number = *str;
-			else if(i == 4)
-				darkest_secret = *str;
+			book_value[i] = str;
 		}
 		return (0);
 	}
+
+	int search_print(int *index)
+	{
+		std::cout <<"|         " << *index << "|";
+		for (int i = 0; i < 3; i++)
+		{
+			if (book_value[i].length() > 10)
+				std::cout<< book_value[i].substr(0, 9) << "." << "|";
+			else
+			{
+				std::cout.width(10);
+				std::cout<< book_value[i] << "|";
+			}
+		}
+		std::cout << std::endl;
+		std::cout << "|-------------------------------------------|" << std::endl;
+
+
+		return (0);
+	}
+
+	void print_book_value(std::string *kind_list)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			std::cout << kind_list[i] << " = " << book_value[i] << std::endl;
+		}
+	}
+
+	void clean_data()
+	{
+		for (int i = 0; i < 5; i++)
+			book_value[i] = "";
+	}
 };
 
-//cout 는 표준 출력 // cin은 표준 입력
+int routine_search(phone_book (*pb)[8], int index, std::string *kind_list)
+{
+	std::string str;
+
+	if (index == 0)
+		std::cout << "# Add a contact before searching !" << std::endl;
+	else
+	{	
+		std::cout << "|-------------------------------------------|\n"
+			<< "|     Index|First Name| Last Name|  Nickname|\n" 
+				<< "|-------------------------------------------|\n";
+		for (int i = 0; i < index && i < 8; i++)
+			(*pb)[i].search_print(&i);
+	
+		while(1)
+		{
+			std::cout << " Enter Index to display Informations or 'N' to Exit" << std::endl;
+			if (input_data(&str) == -1)
+				return (-1);
+			if(str.length() == 1 && isdigit(str[0]) && str[0] < '8' && str[0] < index + '0')
+			{
+				(*pb)[str[0] - '0'].print_book_value(kind_list);
+				break ;
+			}
+			else if (str.length() == 1 && str == "N")
+				break;
+		}
+	}
+	return (0);
+}
+
 int main(void)
 {
 	int index;
@@ -65,19 +118,25 @@ int main(void)
 			break ;
 		if (str == "ADD")
 		{
-			std::cout << "input a new contact’s information" << std::endl;
-			if (pb[index].ADD(kind_list, &str, &index) == -1)
-				break ;
+			if (pb[index % 8].ADD(kind_list, &index) == -1)
+				return (1);
+			std::cout << "complete" << std::endl;
 		}
-//		if (str == "search")
-//		{
-//
-//		}
-//		if (str == "ADD" || str == "EXIT" || str == "search")
-//		{
-//			std::cout << "hi\n";
-//		}
-		std::cout << "complete" << std::endl;
+		else if (str == "SEARCH")
+		{
+			if(routine_search(&pb, index, kind_list) == -1)
+				return (1);
+			std::cout << "complete" << std::endl;
+		}
+		else if (str == "EXIT")
+		{
+			for (int i = 0; i < index && i < 8; i++)
+				pb[i].clean_data();
+			std::cout << "complete" << std::endl;
+			break ;
+		}
+		else
+			std::cout << "> Check command" << std::endl;
 	}
 	return (0);
 }
