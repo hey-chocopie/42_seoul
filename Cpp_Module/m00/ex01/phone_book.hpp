72ae3a3223dc48a5f6e843e8d8e1bfc564c93804
hey-chocopie <ghdyd6372@naver.com>
2021-11-30 15:50:19 +0900

    Add m00 Explanation | Update

--- Cpp_Module/m00/ex01/phone_book.hpp
+++ Cpp_Module/m00/ex01/phone_book.hpp
@@ -10,7 +10,7 @@ class phone_book
 		std::string str;
 		std::string book_value[5];
 	public:
-		static std::string kind_list[5];
+		static std::string kind_list[5]; //static을 쓴이유는, 생성되는 인스턴스 들이, kind_list를 공유하며 사용할려고. static은 프로그램이 실행될 때 만 실행되므로, 인스턴스 새로 만들어도 기존에 만들어둔 kind_list는 그대로 있음. , static은 전역변수로 초기화한다.
 		int ADD(int *index);
 		int search_print(int *index);
 		void print_book_value();
--- Cpp_Module/m00/ex02/tests.cpp
+++ Cpp_Module/m00/ex02/tests.cpp
@@ -22,6 +22,7 @@ int		main( void ) {
 	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
 	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
 	accounts_t				accounts( amounts, amounts + amounts_size );
+	// 이거 잘 모르겟어요.  앞에는 주소 넣고, 뒤에는?
 	accounts_t::iterator	acc_begin	= accounts.begin();
 	accounts_t::iterator	acc_end		= accounts.end();
 
