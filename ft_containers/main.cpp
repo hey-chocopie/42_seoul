ab341cbe1b23f8b77478dfe44cd38687236f3384
Hoyong Lee <hoylee@student.42seoul.kr>
2022-01-04 20:00:26 +0900

    vector construct_range is error

--- ft_containers/main.cpp
+++ ft_containers/main.cpp
@@ -15,6 +15,7 @@ void reconfirm(void)
 	std::cout << RED "check : random_access *a = t" << std::endl;
 	std::cout << RED "check : random_access n + a" << std::endl; //아마 n+a는 n에서 처리해주지 않을까? // 솔직히 난 모르겠다. 이건 넘어가자. 
 	std::cout << RED "check : vector begin, const *a = t" << std::endl;
+	std::cout << RED "check : vector construct, ft::vector<int>::a(50, 50) 이게 주소값에 타버림.." << std::endl;
 }
 
 int main(void)
@@ -28,7 +29,28 @@ int main(void)
 //	ft::vector<int> c(ss.begin(), ss.end());
 	
 
-
+	std::vector<int> ddd(50,48);
+	ft::vector<int> ddd2(50,48);
+	std::cout << ddd2[2] << std::endl;
+	ddd.push_back(12);
+	ddd.push_back(100);
+   std::cout << "The length of storage allocated is now "
+        << ddd.capacity( ) << "." << std::endl;
+	std::vector<int> eee(1000);
+	eee.push_back(120);
+	for(int i = 0; i < 1000; i ++)
+		eee.push_back(120);
+	//eee.push_back(120);
+   std::cout << "The length of storage allocated is now "
+        << eee.capacity( ) << "." << std::endl;
+	std::vector<int> xxx(20000);
+   std::cout << "The length of storage allocated is now "
+        << xxx.capacity( ) << "." << std::endl;
+	std::vector<int> zzz(3000000);
+   std::cout << "The length of storage allocated is now "
+        << zzz.capacity( ) << "." << std::endl;
 	reconfirm();
+
+	
 	return(0);
 }
--- ft_containers/vector.hpp
+++ ft_containers/vector.hpp
@@ -23,10 +23,12 @@ namespace ft
 			typedef typename allocator_type::const_pointer		const_pointer; //    	const T*
 			typedef ptrdiff_t									difference_type;
 			typedef size_t										size_type;
+//			typedef typename allocator_type::pointer			Inputiterator; //    	T*
 
 			allocator_type										_alloc;
 			T*													_array;
 			size_type											_size;
+			size_type											_capacity;
 			//지금 이해안되는게, random_access에서 value_type을 받고 
 			// iterator에서 기본 벡터 만들었을때 iterator 주소는 없음. 만들어야 할것만 만들것.
 			//template<class Category, class T, class Pointer = T*, class Reference = T&>
@@ -41,15 +43,27 @@ namespace ft
 		
 			//default (1) //explicit 원치않는 형변환은 컴파일러 실행
 			explicit vector (const allocator_type& alloc = allocator_type());
+
+
 			//fill (2)
-			explicit vector (size_t n, const value_type& val = value_type(),
-			                 const allocator_type& alloc = allocator_type());
+			explicit vector(size_t n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+
 			//range (3)
-			template <class Inputiterator>
-			         vector (Inputiterator first, Inputiterator last,
+			template <class InputIterator>
+			         vector (InputIterator first, InputIterator last,
 			                 const allocator_type& alloc = allocator_type());
+
 //			//copy (4)
 			vector (const vector& x);
+
+//```
+//		vector<T, A>(size_type n, value_type const & val = value_type());
+//		vector<T, A>(vector<T> const & src);
+//		vector<T, A>(InputIterator first, InputIterator last);
+//```
+
+
+
 			vector& operator= (const vector& rhd); 			
 
 //===========================iterator=======================
@@ -67,46 +81,45 @@ namespace ft
 			size_type size() const;
 			size_type max_size() const;
 
+			size_type capacity() const;
 
 			reference operator[] (size_type n);			
 			const_reference operator[] (size_type n) const;			
+//=======================Modifers=====================
+			template <class InputIterator>
+			void assign (InputIterator first, InputIterator last);
+			void assign (size_type n, const value_type& val);
 	};
 
 
 	/* CONSTRUCTORS + DESTRUCTOR */
 	template <typename T, class Alloc>
-	vector<T, Alloc>::vector(const allocator_type& alloc) : _size(0), _alloc(alloc) //지금 위에서도 빈거 넣었고 _alloc 여기서 또 빈거넣음... 이유가 뭘가?아마 여기 _alloc(alloc)은 지워도 상관없을듯.
+	vector<T, Alloc>::vector(const allocator_type& alloc) : _array(0), _size(0), _alloc(alloc), _capacity(0) //지금 위에서도 빈거 넣었고 _alloc 여기서 또 빈거넣음... 이유가 뭘가?아마 여기 _alloc(alloc)은 지워도 상관없을듯.
 	{
 		//std::cout << 1 << std::endl;
 		//alloc에 뭐가 들어있는지 궁금함. vector<int> a();이렇게 했을때 여기 탈텐데';;
 	}
-			//fill (2)
 
 	template <typename T, class Alloc>
-	vector<T, Alloc>::vector (size_t n, const value_type& val, const allocator_type& alloc) : _size(n), _alloc(alloc)
+	vector<T, Alloc>::vector (size_t n, const value_type& val, const allocator_type& alloc) : _size(n), _alloc(alloc), _capacity(n)
 	{
 		//std::cout << " 2  " << std::endl;
-		_array = _alloc.allocate(_size);
+		_array = _alloc.allocate(_capacity);
 		for (size_t i = 0; i < _size; i++)
 			_alloc.construct(_array + i, val);
 	}
 
+
 	template <typename T, class Alloc>
 	template <class Inputiterator>
     vector<T, Alloc>::vector (Inputiterator first, Inputiterator last,
-        const allocator_type& alloc) : _size(last - first)
+        const allocator_type& alloc) : _alloc(alloc)
 	{
-		_array = alloc.allocate(last - first);
-		for(int cnt = 0; first != last; first++)
-		{
-			std::cout << *first << " " << std::endl;
-			_alloc.construct(_array + cnt, *first);
-			cnt++;
-		}
+		this->assign(first, last);
 	}
 
 	template <typename T, class Alloc>
-	vector<T, Alloc>::vector (const vector& x)
+	vector<T, Alloc>::vector (const vector& x)  
 	{
 		*this = x;
 	}
@@ -126,7 +139,7 @@ namespace ft
 		}
 		this->_size = rhd._size;
 		this->_alloc = rhd._alloc;
-
+		this->_capacity = rhd._capacity;
 		_array = _alloc.allocate(rhd._size);
 		for (size_t i = 0; i < rhd._size; i++)
 			_alloc.construct(_array + i, rhd._array[i]);
@@ -183,13 +196,13 @@ namespace ft
 	template <typename T, class Alloc>
 	typename vector<T, Alloc>::reference vector<T, Alloc>::operator[] (size_type n)
 	{
-		return _alloc[n];
+		return ((this->_array)[n]);
 	}	
 	
 	template <typename T, class Alloc>
 	typename vector<T, Alloc>::const_reference vector<T, Alloc>::operator[] (size_type n) const
 	{
-		return _alloc[n];
+		return ((this->_array)[n]);
 	}
 
 				
@@ -203,6 +216,39 @@ namespace ft
 	{
 		return (_alloc.max_size()); 
 	}
+
+	template <typename T, class Alloc>
+	typename vector<T, Alloc>::size_type vector<T, Alloc>::capacity() const
+	{
+		
+	}
+
+
+	template <typename T, class Alloc>
+	template <class InputIterator>
+	void vector<T, Alloc>::assign (InputIterator first, InputIterator last)
+	{
+		_size = last - first;
+		_capacity = last - first;
+		_array = _alloc.allocate(last - first);
+		for(int i = 0; first != last; first++)
+		{
+			_alloc.construct(_array + i, *first);
+			i++;
+		}
+	}
+
+	template <typename T, class Alloc>
+	void vector<T, Alloc>::assign (size_type n, const value_type& val)
+	{
+		_size = n;
+		_capacity = n;
+		_array = _alloc.allocate(n);
+		for(int i = 0; i < n; i++)
+		{
+			_alloc.construct(_array + i, val);
+		}
+	}//이거 좀더 볼것!
 }
 
 #endif
