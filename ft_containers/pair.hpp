dc39771ee3c9f7273e6c3b6c31470db2b282df72
hey-chocopie <ghdyd6372@naver.com>
2022-02-03 10:23:27 +0900

    수정 : 오타수정

--- ft_containers/pair.hpp
+++ ft_containers/pair.hpp
@@ -6,8 +6,8 @@ namespace ft{
 	struct pair{
 	public:
 		//===========================Member types=======================
-		typedef T1								first_type
-		typedef T2								second_type
+		typedef T1								first_type;
+		typedef T2								second_type;
 
 		first_type								first;
 		second_type								second;
@@ -23,7 +23,7 @@ namespace ft{
 		pair& operator= (const pair& pr);
 
 
-	}
+	};
 	template <class T1, class T2>
 	pair<T1, T2>::pair() : first(), second() {}
 	
@@ -45,10 +45,10 @@ namespace ft{
 	//===========================Non-member function overloads=======================
 	template <class T1, class T2>
 	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-	{ return ((lhs.first == rhs.first) && (lhs.second == rhs.second)) }
+	{ return ((lhs.first == rhs.first) && (lhs.second == rhs.second)); }
 	template <class T1, class T2>
 	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-	{ return !(lhs == rhs) }
+	{ return !(lhs == rhs); }
 	template <class T1, class T2>
 	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
 	{ return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); }
